285fab0ead419261d30de897830c783de91e7e0b
Sheng Liang <sheng@rancher.com>
2016-04-26 12:55:07 +0000

    Added replica remove and rebuild tests

--- stress/stress-test.py
+++ stress/stress-test.py
@@ -1,3 +1,21 @@
+# A stress test program to perform repeated and random-sized operations in multiple processes.
+# 
+# 1. Write data between 0 and BATCH_SIZE blocks at a random offset
+# 2. Read data between 0 and BATCH_SIZE blocks at a random offset
+#
+# We write the 1st 4 bytes of the block the offset, and the 2nd 4 bytes of the block a pattern
+# comprising the current time stamp measured in number of miliseconds since the start of the 
+# program. Every time a pattern is written to the disk, it is also saves in a Python Array
+# called testdata. Testdata is stored in shared memory for all process to access, and
+# is used to validate the correctness of data after being read.
+# Time stamp patterns are reasonably unique and are guaranteed to be in increasing order.
+# Due to race conditions, however, one process might write a pattern, record that pattern in
+# shared memory, only to be immediately over-written by another process. So patterns on disk
+# are not always the same as what's recorded in memory due to race conditions.
+# We use a MAX_TIME_SLACK to account for small differences between disk patterns and in-memory
+# patterns caused by race conditions
+#
+
 import subprocess
 import time
 import cattle
@@ -5,10 +23,8 @@ import copy
 import random
 import sys
 import threading
-from multiprocessing import Process, Manager, Array
-import multiprocessing
-import binascii
 import time
+from multiprocessing import Process, Manager, Array, current_process
 
 SIZE = 20 * 1024 * 1024 * 1024
 SIZE_STR = str(SIZE)
@@ -17,8 +33,8 @@ BATCH_SIZE = 128
 
 INIT_TIME = time.time()
 MAX_SNAPSHOTS = 16
-MAX_TIME_SLACK = 3000
-MAX_BLOCK_OFFSET = SIZE / BLOCK_SIZE
+MAX_TIME_SLACK = 1000
+MAX_BLOCKS = SIZE / BLOCK_SIZE
 
 def gen_blockdata(blockoffset, nblocks, pattern):
   d = bytearray(nblocks * BLOCK_SIZE)
@@ -34,59 +50,59 @@ def gen_blockdata(blockoffset, nblocks, pattern):
   return d
 
 def create_testdata():
-  return Array('i', SIZE / BLOCK_SIZE * (MAX_SNAPSHOTS + 1))
+  return Array('i', MAX_BLOCKS * (MAX_SNAPSHOTS + 1))
 
-def rebuild_replica():
-  if subprocess.check_output('ps -ef'.split()).find("longhorn add") > 0:
-    print "Rebuild replica still in progress, skipping."
-    return
-  if random.random() > 0.5:
-    replica_host = "localhost:9502"
-  else:
-    replica_host = "localhost:9505"
-  print "Rebuild " + replica_host
-  subprocess.call(['./bin/longhorn', 'rm', "tcp://" + replica_host])
-  c = cattle.from_env(url = "http://" + replica_host + "/v1/schemas")
-  for r in c.list_replica():
-    r.open(size=str(SIZE))
-  subprocess.call("nohup ./bin/longhorn add tcp://" + replica_host + " > rebuild.out &", shell = True)
-
+def rebuild_replicas(iterations):
+  for iteration in range(iterations):
+    time.sleep(200)
+    if random.random() > 0.5:
+      replica_host = "localhost:9502"
+    else:
+      replica_host = "localhost:9505"
+    print "Rebuild " + replica_host
+    subprocess.call(['./bin/longhorn', 'rm', "tcp://" + replica_host])
+    c = cattle.from_env(url = "http://" + replica_host + "/v1/schemas")
+    for r in c.list_replica():
+      r.open(size=str(SIZE))
+    subprocess.call(("./bin/longhorn add tcp://" + replica_host).split())
 
 def gen_pattern():
   return int((time.time() - INIT_TIME) * 1000)
 
 def random_write(snapshots, testdata, iterations):
-  print "Starting random write in process " + str(multiprocessing.current_process().pid)
+  proc = current_process()
+  print "Starting random write in " + str(proc) + " pid = " + str(proc.pid)
   fd = open("/dev/longhorn/foo", "r+")
   base = snapshots["livedata"]
   for iteration in range(iterations):
-    if iteration % 100 == 0:
-      print "Iteration " + str(iteration) + " random write in process " + str(multiprocessing.current_process().pid)
-    blockoffset = int(MAX_BLOCK_OFFSET * random.random())
+    if iteration % 1000 == 0:
+      print "Iteration " + str(iteration) + " random write in " + str(proc) + " pid = " + str(proc.pid)
+    blockoffset = int(MAX_BLOCKS * random.random())
     nblocks = int(BATCH_SIZE * random.random())
-    if nblocks + blockoffset > MAX_BLOCK_OFFSET:
-      nblocks = MAX_BLOCK_OFFSET - blockoffset
+    if nblocks + blockoffset > MAX_BLOCKS:
+      nblocks = MAX_BLOCKS - blockoffset
     pattern = gen_pattern()
     for i in range(nblocks):
       testdata[base + blockoffset + i] = pattern
     fd.seek(blockoffset * BLOCK_SIZE)
     fd.write(gen_blockdata(blockoffset, nblocks, pattern))
-  print "Completed random write in process " + str(multiprocessing.current_process().pid)
+  print "Completed random write in " + str(proc) + " pid = " + str(proc.pid)
   fd.close()
 
 def read_and_check(snapshots, testdata, iterations):
   data_blocks = 0
   hole_blocks = 0
-  print "Starting read and check in process " + str(multiprocessing.current_process().pid)
+  proc = current_process()
+  print "Starting read and check in " + str(proc) + " pid = " + str(proc.pid)
   fd = open("/dev/longhorn/foo", "r")
   base = snapshots["livedata"]
   for iteration in range(iterations):
-    if iteration % 100 == 0:
-      print "Iteration " + str(iteration) + " read and check in process " + str(multiprocessing.current_process().pid)
-    blockoffset = int(MAX_BLOCK_OFFSET * random.random())
+    if iteration % 1000 == 0:
+      print "Iteration " + str(iteration) + " read and check in " + str(proc) + " pid = " + str(proc.pid)
+    blockoffset = int(MAX_BLOCKS * random.random())
     nblocks = int(BATCH_SIZE * random.random())
-    if nblocks + blockoffset > MAX_BLOCK_OFFSET:
-      nblocks = MAX_BLOCK_OFFSET - blockoffset
+    if nblocks + blockoffset > MAX_BLOCKS:
+      nblocks = MAX_BLOCKS - blockoffset
     fd.seek(blockoffset * BLOCK_SIZE)
     d = fd.read(BLOCK_SIZE * nblocks)
     current_pattern = gen_pattern()
@@ -94,18 +110,13 @@ def read_and_check(snapshots, testdata, iterations):
       stored_blockoffset = ord(d[BLOCK_SIZE * i + 0]) + (ord(d[BLOCK_SIZE * i + 1]) << 8) + (ord(d[BLOCK_SIZE * i + 2]) << 16) + (ord(d[BLOCK_SIZE * i + 3]) << 24)
       stored_pattern = ord(d[BLOCK_SIZE * i + 4]) + (ord(d[BLOCK_SIZE * i + 5]) << 8) + (ord(d[BLOCK_SIZE * i + 6]) << 16) + (ord(d[BLOCK_SIZE * i + 7]) << 24)
       pattern = testdata[base + blockoffset + i]
+      # Skip entries that are too recent
       if current_pattern - pattern < MAX_TIME_SLACK or current_pattern - stored_pattern < MAX_TIME_SLACK:
         continue
       try:
-        if pattern != 0 and blockoffset != 0:
+        if stored_pattern != 0 or stored_blockoffset != 0:
           assert stored_blockoffset == blockoffset + i
-          if stored_pattern > pattern:
-            diff = stored_pattern - pattern
-          else:
-            diff = pattern - stored_pattern
-#          if diff > 0:
-#            print "Difference at " + str(stored_blockoffset) + " is " + str(diff)
-          assert diff < MAX_TIME_SLACK
+          assert abs(stored_pattern - pattern) < MAX_TIME_SLACK
           data_blocks = data_blocks + 1
         else:
           assert stored_blockoffset == 0
@@ -115,7 +126,7 @@ def read_and_check(snapshots, testdata, iterations):
         print "current_pattern = " + str(current_pattern) + " nblocks = " + str(nblocks) + " blockoffset = " + str(blockoffset) + " i = " + str(i) + " stored_blockoffset = " + str(stored_blockoffset) + " pattern = " + str(pattern) + " stored_pattern = " + str(stored_pattern)
         raise
   print "data_blocks: " + str(data_blocks) + " hole_blocks: " + str(hole_blocks)
-  print "Completed read and check in process " + str(multiprocessing.current_process().pid)
+  print "Completed read and check in " + str(proc) + " pid = " + str(proc.pid)
   fd.close()
     
 
@@ -147,6 +158,11 @@ for i in range(10):
   p = Process(target = read_and_check, args = (snapshots, testdata, 10000))
   workers.append(p)
   p.start()
+
+p = Process(target = rebuild_replicas, args = (8,))
+workers.append(p)
+p.start()
+
 
 for p in workers:
   p.join()
