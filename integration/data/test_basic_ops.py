e4b517375a3d8620e6f6f36c125ffc7ee7a268c3
kirill <kirill@rancher.com>
2016-06-16 14:14:41 -0700

    controller: handle read/write beyond volume boundaries - the condition is handled inside the controller to prevent replicas from being kicked out - issue #131 - enabled corresponding test

--- integration/data/test_basic_ops.py
+++ integration/data/test_basic_ops.py
@@ -16,9 +16,13 @@ def test_basic_rw(dev):  # NOQA
         common.verify_data(dev, offset, data)
 
 
-# TODO BUG: https://github.com/rancher/longhorn/issues/131
-@pytest.mark.xfail(strict=True)  # NOQA
-def test_beyond_bounary(dev):
+# See also BUG: https://github.com/rancher/longhorn/issues/131
+def test_beyond_boundary(dev):  # NOQA
+    # check write at the boundary
+    data = common.random_string(128)
+    common.verify_data(dev, SIZE - 128, data)
+
+    # out of bounds
     with pytest.raises(cattle.ApiError) as err:
         write_dev(dev, SIZE, "1")
         assert 'EOF' in str(err.value)
@@ -26,6 +30,7 @@ def test_beyond_bounary(dev):
         read_dev(dev, SIZE, 1)
         assert 'EOF' in str(err.value)
 
+    # normal writes to verify controller/replica survival
     for i in range(0, 10):
         offset = random.randint(0, SIZE - 256)
         length = random.randint(0, 256)
