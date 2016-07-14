a6a89b540483a48acdd6db1b901d15481be5ec90
Sheng Yang <sheng@yasker.org>
2016-07-13 18:37:54 -0700

    fuse: When Activate was called twice, reinitialize the frontend
    It's possible that backend changed.
    
    We didn't enforce frontend to shutdown when no backend exists. Should do that
    in the future.

--- frontend/fusedev/frontend.go
+++ frontend/fusedev/frontend.go
@@ -13,6 +13,12 @@ type Fuse struct {
 }
 
 func (f *Fuse) Activate(name string, size, sectorSize int64, rw types.ReaderWriterAt) error {
+	if f.fs != nil {
+		log.Infof("FUSE frontend already activated for %v, shut it down first", name)
+		if err := f.Shutdown(); err != nil {
+			return err
+		}
+	}
 	log.Infof("Activate FUSE frontend for %v, size %v, sector size %v", name, size, sectorSize)
 	fs, err := start(name, size, sectorSize, rw)
 	if err != nil {
@@ -27,5 +33,9 @@ func (f *Fuse) Shutdown() error {
 		return nil
 	}
 	log.Infof("Shutdown FUSE frontend for %v", f.fs.Volume)
-	return f.fs.Stop()
+	if err := f.fs.Stop(); err != nil {
+		return err
+	}
+	f.fs = nil
+	return nil
 }
