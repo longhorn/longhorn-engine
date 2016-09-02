8f4c125c942a3e78407e1ba2dd14807f8f91dbe4
Jimeng Liu <jimeng@rancher.com>
2016-09-02 11:26:01 -0700

    no errno needed

--- vendor/github.com/rancher/sparse-tools/sparse/file.go
+++ vendor/github.com/rancher/sparse-tools/sparse/file.go
@@ -223,7 +223,7 @@ func GetFiemapRegionExts(file FileIoProcessor, interval Interval) ([]Extent, err
 	// last ext should have the FIEMAP_EXTENT_LAST set, otherwise we don't get all exts
 	if exts[len(exts)-1].Flags&FIEMAP_EXTENT_LAST == 0 {
 		fileInfo, _ := file.Stat()
-		extCount, _, errno := fiemap.FiemapRegion(0, uint64(0), uint64(fileInfo.Size()))
+		extCount, _, _ := fiemap.FiemapRegion(0, uint64(0), uint64(fileInfo.Size()))
 		return exts, fmt.Errorf("The exts returned by fiemap are not complete, file ext count: %d, region ext count: %d", extCount, len(exts))
 	}
 	return exts, nil
