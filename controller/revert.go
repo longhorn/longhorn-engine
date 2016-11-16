1557011b05730942260550fee9afa0b7590ae61e
Sheng Yang <sheng@yasker.org>
2016-11-15 18:36:24 -0800

    controller: Allow revert if at least one replica is in RW mode
    Also return success if even only one replica success in revert
    
    But doesn't allow revert if one replica is in WO mode.
    
    Issue #243

--- controller/revert.go
+++ controller/revert.go
@@ -10,10 +10,23 @@ import (
 )
 
 func (c *Controller) Revert(name string) error {
+	doable := false
+	rw := false
+	wo := false
 	for _, rep := range c.replicas {
-		if rep.Mode != types.RW {
-			return fmt.Errorf("Replica %s is in mode %s", rep.Address, rep.Mode)
+		//BUG: Need to deal with replica in rebuilding(WO) mode.
+		if rep.Mode == types.RW {
+			rw = true
+		} else if rep.Mode == types.WO {
+			wo = true
 		}
+	}
+	if rw && !wo {
+		doable = true
+	}
+
+	if !doable {
+		return fmt.Errorf("Not valid state to revert, rebuilding in process or all replica are in ERR state")
 	}
 
 	clients, name, err := c.clientsAndSnapshot(name)
@@ -28,12 +41,20 @@ func (c *Controller) Revert(name string) error {
 	c.Lock()
 	defer c.Unlock()
 
+	minimalSuccess := false
 	for address, client := range clients {
 		logrus.Infof("Reverting to snapshot %s on %s", name, address)
 		if err := client.Revert(name); err != nil {
+			logrus.Errorf("Error on reverting to %s on %s: %v", name, address, err)
 			c.setReplicaModeNoLock(address, types.ERR)
-			return err
+		} else {
+			minimalSuccess = true
+			logrus.Infof("Reverting to snapshot %s on %s successed", name, address)
 		}
+	}
+
+	if !minimalSuccess {
+		return fmt.Errorf("Fail to revert to %v on all replicas", name)
 	}
 
 	return c.startFrontend()
@@ -43,6 +64,13 @@ func (c *Controller) clientsAndSnapshot(name string) (map[string]*client.Replica
 	clients := map[string]*client.ReplicaClient{}
 
 	for _, replica := range c.replicas {
+		if replica.Mode == types.WO {
+			return nil, "", fmt.Errorf("Cannot revert %s during rebuilding process", replica.Address)
+		}
+		if replica.Mode != types.RW {
+			continue
+		}
+
 		if !strings.HasPrefix(replica.Address, "tcp://") {
 			return nil, "", fmt.Errorf("Backend %s does not support revert", replica.Address)
 		}
