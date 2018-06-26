cebde5c431fce6ed43ba89ff182bf280e0181498
Sheng Yang <sheng@yasker.org>
2018-06-26 14:37:24 -0700

    integration: Enhance data test

--- integration/data/test_snapshot.py
+++ integration/data/test_snapshot.py
@@ -2,37 +2,31 @@ import cmd
 import common
 from common import dev, backing_dev  # NOQA
 from common import read_dev, read_from_backing_file, VOLUME_HEAD
+from common import Snapshot, generate_random_data
 from snapshot_tree import snapshot_tree_build, snapshot_tree_verify_node
 
 
 def test_snapshot_revert(dev):  # NOQA
-    offset = 0
-    length = 128
+    existings = {}
 
-    snap1_data = common.random_string(length)
-    common.verify_data(dev, offset, snap1_data)
-    snap1 = cmd.snapshot_create()
-
-    snap2_data = common.random_string(length)
-    common.verify_data(dev, offset, snap2_data)
-    snap2 = cmd.snapshot_create()
-
-    snap3_data = common.random_string(length)
-    common.verify_data(dev, offset, snap3_data)
-    snap3 = cmd.snapshot_create()
+    snap1 = Snapshot(dev, generate_random_data(existings))
+    snap2 = Snapshot(dev, generate_random_data(existings))
+    snap3 = Snapshot(dev, generate_random_data(existings))
 
     snapList = cmd.snapshot_ls()
-    assert snap1 in snapList
-    assert snap2 in snapList
-    assert snap3 in snapList
+    assert snap1.name in snapList
+    assert snap2.name in snapList
+    assert snap3.name in snapList
 
-    cmd.snapshot_revert(snap2)
-    readed = read_dev(dev, offset, length)
-    assert readed == snap2_data
+    cmd.snapshot_revert(snap2.name)
+    snap3.refute_data()
+    snap2.verify_checksum()
+    snap1.verify_data()
 
-    cmd.snapshot_revert(snap1)
-    readed = read_dev(dev, offset, length)
-    assert readed == snap1_data
+    cmd.snapshot_revert(snap1.name)
+    snap3.refute_data()
+    snap2.refute_data()
+    snap1.verify_checksum()
 
 
 # BUG: https://github.com/rancher/longhorn/issues/108
