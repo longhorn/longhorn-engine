7094d43f66ba15d25563d265f0c53afaf4966055
Shuo Wu <shuo.wu.career@gmail.com>
2019-06-05 14:44:41 -0700

    remove replica http server and client

--- app/replica.go
+++ app/replica.go
@@ -3,7 +3,6 @@ package app
 import (
 	"errors"
 	"net"
-	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -16,7 +15,6 @@ import (
 	"google.golang.org/grpc/reflection"
 
 	"github.com/longhorn/longhorn-engine/replica"
-	"github.com/longhorn/longhorn-engine/replica/rest"
 	"github.com/longhorn/longhorn-engine/replica/rpc"
 	"github.com/longhorn/longhorn-engine/util"
 )
@@ -92,7 +90,7 @@ func startReplica(c *cli.Context) error {
 		}
 	}
 
-	controlAddress, dataAddress, syncAddress, replicaServiceURL, err := util.ParseAddresses(address)
+	controlAddress, dataAddress, syncAddress, err := util.ParseAddresses(address)
 	if err != nil {
 		return err
 	}
@@ -100,22 +98,9 @@ func startReplica(c *cli.Context) error {
 	resp := make(chan error)
 
 	go func() {
-		server := rest.NewServer(s)
-		router := http.Handler(rest.NewRouter(server))
-		router = util.FilteredLoggingHandler(map[string]struct{}{
-			"/ping":          {},
-			"/v1":            {},
-			"/v1/replicas/1": {},
-		}, os.Stdout, router)
-		logrus.Infof("Listening on control %s", controlAddress)
-		err := http.ListenAndServe(controlAddress, router)
-		logrus.Warnf("Replica rest server at %v is down: %v", controlAddress, err)
-	}()
-
-	go func() {
-		listen, err := net.Listen("tcp", replicaServiceURL)
+		listen, err := net.Listen("tcp", controlAddress)
 		if err != nil {
-			logrus.Warnf("Failed to listen %v: %v", replicaServiceURL, err)
+			logrus.Warnf("Failed to listen %v: %v", controlAddress, err)
 			resp <- err
 			return
 		}
@@ -124,9 +109,9 @@ func startReplica(c *cli.Context) error {
 		rpc.RegisterReplicaServiceServer(server, rpc.NewReplicaServer(s))
 		reflection.Register(server)
 
-		logrus.Infof("Listening on gRPC Replica server %s", replicaServiceURL)
+		logrus.Infof("Listening on gRPC Replica server %s", controlAddress)
 		err = server.Serve(listen)
-		logrus.Warnf("gRPC Replica server at %v is down: %v", replicaServiceURL, err)
+		logrus.Warnf("gRPC Replica server at %v is down: %v", controlAddress, err)
 		resp <- err
 	}()
 
