f14f68730fc59e936c31e81503c615c23b1c2d6e
Sheng Yang <sheng@yasker.org>
2019-06-15 20:47:58 -0700

    test: launcher: add test for two volumes
    Now it triggers tgt panic which is expected:
    
    (null): ipc_init(787) another tgtd is using /var/run/tgtd/socket.0.lock
    go-iscsi-helper: command failed: exit status 1
    tgtd: tgt_target_create(2154) Target id 1 already exists
    panic: exit status 1
    
    goroutine 32 [running]:
    github.com/longhorn/longhorn-engine-launcher/vendor/github.com/longhorn/go-iscsi-helper/iscsi.startDaemon(0xc4201e60c0, 0x0)
    	/go/src/github.com/longhorn/longhorn-engine-launcher/vendor/github.com/longhorn/go-iscsi-helper/iscsi/target.go:196 +0x3a4
    created by github.com/longhorn/longhorn-engine-launcher/vendor/github.com/longhorn/go-iscsi-helper/iscsi.StartDaemon
    	/go/src/github.com/longhorn/longhorn-engine-launcher/vendor/github.com/longhorn/go-iscsi-helper/iscsi/target.go:164 +0x7d
    INFO[0050] Process update: controller1: state error: Error: exit status 2

--- integration/launcher/test_launcher_basic.py
+++ integration/launcher/test_launcher_basic.py
@@ -145,3 +145,87 @@ def test_one_volume():
 
     ps = process_list()
     assert(len(ps) == 0)
+
+
+def test_two_volumes():
+    rs = process_list()
+    assert(len(rs) == 0)
+
+    listen = "--listen,localhost:"
+    port_count = 15
+    name_base = "replica"
+
+    for i in range(2):
+        tmp_dir = tempfile.mkdtemp()
+        name = name_base + str(i)
+        r = process_create(name, ["replica", tmp_dir, "--size", str(SIZE)],
+                           port_count=port_count, port_args=[listen])
+        assert(r["name"] == name)
+        assert(r["state"] == PROC_STATE_RUNNING)
+
+        r = process_get(name)
+        assert(r["name"] == name)
+        assert(r["state"] == PROC_STATE_RUNNING)
+
+        rs = process_list()
+        assert(len(rs) == i+1)
+        assert(name in rs)
+        assert(rs[name]["name"] == name)
+        assert(rs[name]["state"] == PROC_STATE_RUNNING)
+
+    ps = process_list()
+    replica_args = []
+    for r in ps:
+        replica_args.append(["--replica",
+                            "tcp://localhost:"+str(ps[r]["portStart"])])
+
+    controller_launcher_listen = "--launcher-listen,localhost:"
+    controller_listen = "--listen,localhost:"
+    controller_port_count = 2
+
+    cname_base = "controller"
+    volume_name_base = "testvol"
+
+    for i in range(2):
+        name = cname_base + str(i)
+        volname = volume_name_base + str(i)
+        c = process_create(name,
+                           ["start", volname,
+                            "--longhorn-binary", cmd.LONGHORN_BINARY,
+                            "--size", str(SIZE),
+                            "--frontend", "tgt-blockdev"] + replica_args[i],
+                           port_count=controller_port_count,
+                           port_args=[controller_launcher_listen,
+                                      controller_listen],
+                           binary="/usr/local/bin/longhorn-engine-launcher")
+        assert(c["name"] == name)
+        assert(c["state"] == PROC_STATE_RUNNING)
+
+        found = False
+        for i in range(RETRY_COUNTS):
+            if os.path.exists(os.path.join("/dev/longhorn/", volname)):
+                found = True
+                break
+            time.sleep(RETRY_INTERVAL)
+        assert found
+
+    ps = process_list()
+    assert(len(ps) == 4)
+
+    for i in range(2):
+        name = cname_base + str(i)
+        c = process_delete(name)
+        assert(c["name"] == name)
+
+    ps = process_list()
+    assert(len(ps) == 2)
+
+    for i in range(2):
+        name = name_base + str(i)
+        r = process_delete(name)
+        assert(r["name"] == name)
+
+        wait_for_process_deletion(name)
+
+    ps = process_list()
+    assert(len(ps) == 0)
