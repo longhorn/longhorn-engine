622021ea20447b05227d66f30aa1d58cdc967157
Shuo Wu <shuo.wu.career@gmail.com>
2019-06-20 11:02:55 -0700

    Add and modify rpc calls
    Add EngineList rpc call

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-protoc -I rpc/ rpc/rpc.proto --go_out=plugins=grpc:rpc
+mkdir -p $GOPATH/src/github.com/protocolbuffers && git clone https://github.com/protocolbuffers/protobuf.git $GOPATH/src/github.com/protocolbuffers/protobuf
+
+protoc -I rpc/ -I $GOPATH/src/github.com/protocolbuffers/protobuf/src/ rpc/rpc.proto --go_out=plugins=grpc:rpc
