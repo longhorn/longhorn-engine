d3dc51c361c7aa3f076a82713b3ca695d015d5ad
Shreesha Rao <shreesha@rancher.com>
2019-06-24 22:34:31 +0000

    Updating the vendor lib for backupstore

--- vendor/github.com/longhorn/backupstore/deltablock.go
+++ vendor/github.com/longhorn/backupstore/deltablock.go
@@ -20,6 +20,13 @@ type DeltaBackupConfig struct {
 	Labels   map[string]string
 }
 
+type DeltaRestoreConfig struct {
+	BackupURL      string
+	DeltaOps       DeltaRestoreOperations
+	LastBackupName string
+	Filename       string
+}
+
 type BlockMapping struct {
 	Offset        int64
 	BlockChecksum string
@@ -32,6 +39,10 @@ type DeltaBlockBackupOperations interface {
 	ReadSnapshot(id, volumeID string, start int64, data []byte) error
 	CloseSnapshot(id, volumeID string) error
 	UpdateBackupStatus(id, volumeID string, backupProgress int, backupURL string, err string) error
+}
+
+type DeltaRestoreOperations interface {
+	UpdateRestoreStatus(snapshot string, restoreProgress int, err error)
 }
 
 const (
@@ -285,7 +296,18 @@ func mergeSnapshotMap(deltaBackup, lastBackup *Backup) *Backup {
 	return backup
 }
 
-func RestoreDeltaBlockBackup(backupURL, volDevName string) error {
+func RestoreDeltaBlockBackup(config *DeltaRestoreConfig) error {
+	if config == nil {
+		return fmt.Errorf("invalid empty config for restore")
+	}
+
+	volDevName := config.Filename
+	backupURL := config.BackupURL
+	deltaOps := config.DeltaOps
+	if deltaOps == nil {
+		return fmt.Errorf("missing DeltaRestoreOperations")
+	}
+
 	bsDriver, err := GetBackupStoreDriver(backupURL)
 	if err != nil {
 		return err
@@ -312,7 +334,6 @@ func RestoreDeltaBlockBackup(backupURL, volDevName string) error {
 	if err != nil {
 		return err
 	}
-	defer volDev.Close()
 
 	stat, err := volDev.Stat()
 	if err != nil {
@@ -333,21 +354,29 @@ func RestoreDeltaBlockBackup(backupURL, volDevName string) error {
 		LogFieldVolumeDev:  volDevName,
 		LogEventBackupURL:  backupURL,
 	}).Debug()
-	blkCounts := len(backup.Blocks)
-	for i, block := range backup.Blocks {
-		log.Debugf("Restore for %v: block %v, %v/%v", volDevName, block.BlockChecksum, i+1, blkCounts)
-		if err := restoreBlockToFile(srcVolumeName, volDev, bsDriver, block); err != nil {
-			return err
-		}
-	}
 
-	// We want to truncate regular files, but not device
-	if stat.Mode()&os.ModeType == 0 {
-		log.Debugf("Truncate %v to size %v", volDevName, vol.Size)
-		if err := volDev.Truncate(vol.Size); err != nil {
-			return err
+	go func() {
+		defer volDev.Close()
+		blkCounts := len(backup.Blocks)
+		var progress int
+		for i, block := range backup.Blocks {
+			log.Debugf("Restore for %v: block %v, %v/%v", volDevName, block.BlockChecksum, i+1, blkCounts)
+			if err := restoreBlockToFile(srcVolumeName, volDev, bsDriver, block); err != nil {
+				deltaOps.UpdateRestoreStatus(volDevName, progress, err)
+			}
+			progress = int((float64(i+1) / float64(blkCounts)) * PROGRESS_PERCENTAGE_BACKUP_SNAPSHOT)
+			deltaOps.UpdateRestoreStatus(volDevName, progress, err)
 		}
-	}
+
+		// We want to truncate regular files, but not device
+		if stat.Mode()&os.ModeType == 0 {
+			log.Debugf("Truncate %v to size %v", volDevName, vol.Size)
+			if err := volDev.Truncate(vol.Size); err != nil {
+				deltaOps.UpdateRestoreStatus(volDevName, progress, err)
+			}
+		}
+		deltaOps.UpdateRestoreStatus(volDevName, PROGRESS_PERCENTAGE_BACKUP_TOTAL, nil)
+	}()
 
 	return nil
 }
