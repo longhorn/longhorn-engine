e06f54ae4984e7eef17d48688813506f5d09fb88
Shuo Wu <shuo.wu.career@gmail.com>
2019-07-12 11:00:13 -0700

    Add vendor_proto, then update gRPC script and code for imported Empty struct

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-protoc -I rpc/ -I vendor/github.com/golang/protobuf/ptypes/empty rpc/rpc.proto --go_out=plugins=grpc:rpc
+# check and download dependency for gRPC code generate
+if [ ! -e ./vendor_proto/protobuf/src/google/protobuf ]; then
+    rm -rf ./vendor_proto/protobuf/ &&
+    git clone https://github.com/protocolbuffers/protobuf.git ./vendor_proto/protobuf/ &&
+    cd ./vendor_proto/protobuf/ &&
+    git checkout tags/v3.9.0 &&
+    find . ! -regex './src/google/protobuf.*' -delete &&
+    cd ../../
+fi
+
+protoc -I rpc/ -I vendor_proto/protobuf/src/ rpc/rpc.proto --go_out=plugins=grpc:rpc
