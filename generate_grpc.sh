ce069d6c332288856e86830e3a989bb8331a99de
Shuo Wu <shuo.wu.career@gmail.com>
2019-07-12 11:00:13 -0700

    Update gRPC script and code for imported Empty struct

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-mkdir -p $GOPATH/src/github.com/protocolbuffers && git clone https://github.com/protocolbuffers/protobuf.git $GOPATH/src/github.com/protocolbuffers/protobuf
-
-protoc -I rpc/ -I $GOPATH/src/github.com/protocolbuffers/protobuf/src/ rpc/rpc.proto --go_out=plugins=grpc:rpc
+protoc -I rpc/ -I vendor/github.com/golang/protobuf/ptypes/empty rpc/rpc.proto --go_out=plugins=grpc:rpc
