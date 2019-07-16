34c87651839d7f36a8ad78e55a7beb6abbe319f1
Yijie Wei <ywei88@gatech.edu>
2019-07-16 16:14:18 -0700

    Backup volume deletion: update vendor

--- vendor/github.com/longhorn/backupstore/cmd/remove.go
+++ vendor/github.com/longhorn/backupstore/cmd/remove.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/urfave/cli"
 
 	"github.com/longhorn/backupstore"
@@ -11,8 +12,14 @@ func BackupRemoveCmd() cli.Command {
 	return cli.Command{
 		Name:    "remove",
 		Aliases: []string{"rm", "delete"},
-		Usage:   "remove a backup in objectstore: rm <backup>",
-		Action:  cmdBackupRemove,
+		Usage:   "remove a backup or backup volume in objectstore: rm <backup>",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "volume",
+				Usage: "volume name, only use it when deleting a backup volume with dest URL",
+			},
+		},
+		Action: cmdBackupRemove,
 	}
 }
 
@@ -24,16 +31,26 @@ func cmdBackupRemove(c *cli.Context) {
 
 func doBackupRemove(c *cli.Context) error {
 	if c.NArg() == 0 {
-		return RequiredMissingError("backup URL")
+		return RequiredMissingError("dest URL")
 	}
-	backupURL := c.Args()[0]
-	if backupURL == "" {
-		return RequiredMissingError("backup URL")
+	destURL := c.Args()[0]
+	if destURL == "" {
+		return RequiredMissingError("dest URL")
 	}
-	backupURL = util.UnescapeURL(backupURL)
 
-	if err := backupstore.DeleteDeltaBlockBackup(backupURL); err != nil {
-		return err
+	volumeName := c.String("volume")
+	if volumeName == "" {
+		destURL = util.UnescapeURL(destURL)
+		if err := backupstore.DeleteDeltaBlockBackup(destURL); err != nil {
+			return err
+		}
+	} else {
+		if !util.ValidateName(volumeName) {
+			return fmt.Errorf("invalid backup volume name %v", volumeName)
+		}
+		if err := backupstore.DeleteBackupVolume(volumeName, destURL); err != nil {
+			return err
+		}
 	}
 	return nil
 }
