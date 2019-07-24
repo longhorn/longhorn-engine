a2a072669b4f00fddef22ecbef7df3897f09ab6a
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-07-24 16:51:14 -0700

    Update longhorn-instance-manager in dapper

--- vendor/github.com/longhorn/longhorn-instance-manager/client/process_manager.go
+++ vendor/github.com/longhorn/longhorn-instance-manager/client/process_manager.go
@@ -3,6 +3,8 @@ package client
 import (
 	"fmt"
 
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/pkg/errors"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
@@ -19,25 +21,6 @@ func NewProcessManagerClient(address string) *ProcessManagerClient {
 	return &ProcessManagerClient{
 		Address: address,
 	}
-}
-
-func RPCToProcess(obj *rpc.ProcessResponse) *api.Process {
-	return &api.Process{
-		Name:          obj.Spec.Name,
-		Binary:        obj.Spec.Binary,
-		Args:          obj.Spec.Args,
-		PortCount:     obj.Spec.PortCount,
-		PortArgs:      obj.Spec.PortArgs,
-		ProcessStatus: RPCToProcessStatus(obj.Status),
-	}
-}
-
-func RPCToProcessList(obj *rpc.ProcessListResponse) map[string]*api.Process {
-	ret := map[string]*api.Process{}
-	for name, p := range obj.Processes {
-		ret[name] = RPCToProcess(p)
-	}
-	return ret
 }
 
 func (cli *ProcessManagerClient) ProcessCreate(name, binary string, portCount int, args, portArgs []string) (*api.Process, error) {
@@ -67,7 +50,7 @@ func (cli *ProcessManagerClient) ProcessCreate(name, binary string, portCount in
 	if err != nil {
 		return nil, fmt.Errorf("failed to start process: %v", err)
 	}
-	return RPCToProcess(p), nil
+	return api.RPCToProcess(p), nil
 }
 
 func (cli *ProcessManagerClient) ProcessDelete(name string) (*api.Process, error) {
@@ -91,7 +74,7 @@ func (cli *ProcessManagerClient) ProcessDelete(name string) (*api.Process, error
 	if err != nil {
 		return nil, fmt.Errorf("failed to delete process %v: %v", name, err)
 	}
-	return RPCToProcess(p), nil
+	return api.RPCToProcess(p), nil
 }
 
 func (cli *ProcessManagerClient) ProcessGet(name string) (*api.Process, error) {
@@ -115,7 +98,7 @@ func (cli *ProcessManagerClient) ProcessGet(name string) (*api.Process, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to get process %v: %v", name, err)
 	}
-	return RPCToProcess(p), nil
+	return api.RPCToProcess(p), nil
 }
 
 func (cli *ProcessManagerClient) ProcessList() (map[string]*api.Process, error) {
@@ -133,7 +116,7 @@ func (cli *ProcessManagerClient) ProcessList() (map[string]*api.Process, error) 
 	if err != nil {
 		return nil, fmt.Errorf("failed to list processes: %v", err)
 	}
-	return RPCToProcessList(ps), nil
+	return api.RPCToProcessList(ps), nil
 }
 
 func (cli *ProcessManagerClient) ProcessLog(name string) (*api.LogStream, error) {
@@ -145,17 +128,32 @@ func (cli *ProcessManagerClient) ProcessLog(name string) (*api.LogStream, error)
 	if err != nil {
 		return nil, fmt.Errorf("cannot connect process manager service to %v: %v", cli.Address, err)
 	}
-	defer conn.Close()
 
 	client := rpc.NewProcessManagerServiceClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), types.GRPCServiceTimeout)
-	defer cancel()
-
 	stream, err := client.ProcessLog(ctx, &rpc.LogRequest{
 		Name: name,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to get process log of %v: %v", name, err)
 	}
-	return api.NewLogStream(stream), nil
+	return api.NewLogStream(conn, cancel, stream), nil
+}
+
+func (cli *ProcessManagerClient) ProcessWatch() (*api.ProcessStream, error) {
+	conn, err := grpc.Dial(cli.Address, grpc.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("cannot connect process manager service to %v: %v", cli.Address, err)
+	}
+
+	// Don't cleanup the Client here, we don't know when the user will be done with the Stream. Pass it to the wrapper
+	// and allow the user to take care of it.
+	client := rpc.NewProcessManagerServiceClient(conn)
+	ctx, cancel := context.WithCancel(context.Background())
+	stream, err := client.ProcessWatch(ctx, &empty.Empty{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to open process update stream")
+	}
+
+	return api.NewProcessStream(conn, cancel, stream), nil
 }
