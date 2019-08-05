a4f5c9a670b4f17c876f5f7b4524a76863fa927c
Sheng Yang <sheng@yasker.org>
2019-08-05 03:00:03 -0700

    Update instance manager
    To f2180b80b696731ba2c092c2c1d964c20b2d23a9
    
    Fixed some deadlocks.

--- vendor/github.com/longhorn/longhorn-instance-manager/types/types.go
+++ vendor/github.com/longhorn/longhorn-instance-manager/types/types.go
@@ -1,16 +1,20 @@
 package types
 
-import "time"
+import (
+	"time"
+)
 
 const (
 	GRPCServiceTimeout = 1 * time.Minute
-
-	WaitInterval = time.Second
-	WaitCount    = 60
 
 	ProcessStateRunning  = "running"
 	ProcessStateStarting = "starting"
 	ProcessStateStopped  = "stopped"
 	ProcessStateStopping = "stopping"
 	ProcessStateError    = "error"
+)
+
+var (
+	WaitInterval = time.Second
+	WaitCount    = 60
 )
