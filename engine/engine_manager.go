9c4096ffb9ec3495e53a18ba3a21c2d311b6e13f
Sheng Yang <sheng@yasker.org>
2019-08-07 11:32:47 -0700

    engine: move process_stream_wrapper to a separate file

--- engine/engine_manager.go
+++ engine/engine_manager.go
@@ -11,7 +11,6 @@ import (
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"
 
 	"github.com/longhorn/longhorn-instance-manager/rpc"
 	"github.com/longhorn/longhorn-instance-manager/types"
@@ -31,45 +30,9 @@ type Manager struct {
 	tIDAllocator    *util.Bitmap
 }
 
-type ProcessStreamWrapper struct {
-	grpc.ServerStream
-	*sync.RWMutex
-
-	updateChs map[chan<- *rpc.ProcessResponse]struct{}
-}
-
 const (
 	MaxTgtTargetNumber = 4096
 )
-
-func NewProcessStreamWrapper() *ProcessStreamWrapper {
-	return &ProcessStreamWrapper{
-		RWMutex:   &sync.RWMutex{},
-		updateChs: make(map[chan<- *rpc.ProcessResponse]struct{}),
-	}
-}
-
-func (sw ProcessStreamWrapper) Send(response *rpc.ProcessResponse) error {
-	sw.RLock()
-	for ch := range sw.updateChs {
-		ch <- response
-	}
-	sw.RUnlock()
-	return nil
-}
-
-func (sw *ProcessStreamWrapper) AddLauncherStream(updateCh chan<- *rpc.ProcessResponse) {
-	sw.Lock()
-	sw.updateChs[updateCh] = struct{}{}
-	sw.Unlock()
-}
-
-func (sw *ProcessStreamWrapper) RemoveLauncherStream(updateCh chan<- *rpc.ProcessResponse) {
-	sw.Lock()
-	delete(sw.updateChs, updateCh)
-	sw.Unlock()
-	close(updateCh)
-}
 
 func NewEngineManager(pm rpc.ProcessManagerServiceServer, listen string, shutdownCh chan error) (*Manager, error) {
 	em := &Manager{
