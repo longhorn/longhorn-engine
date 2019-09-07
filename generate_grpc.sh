010a320b464ec75982e7ab5464c3601e6172d3c7
Sheng Yang <sheng.yang@rancher.com>
2019-09-07 12:35:27 -0700

    build: rpc: Only download empty.proto
    Signed-off-by: Sheng Yang <sheng.yang@rancher.com>

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # check and download dependency for gRPC code generate
+set -e
+
+# check and download dependency for gRPC code generate
 if [ ! -e ./vendor_proto/protobuf/src/google/protobuf ]; then
-    rm -rf ./vendor_proto/protobuf/ &&
-    git clone https://github.com/protocolbuffers/protobuf.git ./vendor_proto/protobuf/ &&
-    cd ./vendor_proto/protobuf/ &&
-    git checkout tags/v3.9.0 &&
-    find . ! -regex './src/google/protobuf.*' -delete &&
-    cd ../../
+    rm -rf ./vendor_proto/protobuf/src/google/protobuf
+    DIR="./vendor_proto/protobuf/src/google/protobuf"
+    mkdir -p $DIR
+    wget https://raw.githubusercontent.com/protocolbuffers/protobuf/v3.9.0/src/google/protobuf/empty.proto -P $DIR
 fi
 
 protoc -I rpc/ -I vendor_proto/protobuf/src/ rpc/rpc.proto --go_out=plugins=grpc:rpc
