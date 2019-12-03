579ba10a26920d7667b5c39be725f20f74a0aa6f
Shuo Wu <shuo@rancher.com>
2019-12-03 12:14:52 -0800

    instancemanager: Add a new gRPC call EngineExpand()
    Signed-off-by: Shuo Wu <shuo@rancher.com>

--- integration/rpc/instance_manager/rpc_pb2.py
+++ integration/rpc/instance_manager/rpc_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\trpc.proto\x1a\x1bgoogle/protobuf/empty.proto\"\xa6\x01\n\nEngineSpec\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x13\n\x0bvolume_name\x18\x02 \x01(\t\x12\x0e\n\x06\x62inary\x18\x03 \x01(\t\x12\x0e\n\x06listen\x18\x04 \x01(\t\x12\x11\n\tlisten_ip\x18\x05 \x01(\t\x12\x0c\n\x04size\x18\x06 \x01(\x03\x12\x10\n\x08\x66rontend\x18\x07 \x01(\t\x12\x10\n\x08\x62\x61\x63kends\x18\x08 \x03(\t\x12\x10\n\x08replicas\x18\t \x03(\t\"H\n\x0c\x45ngineStatus\x12&\n\x0eprocess_status\x18\x01 \x01(\x0b\x32\x0e.ProcessStatus\x12\x10\n\x08\x65ndpoint\x18\x02 \x01(\t\"[\n\x0e\x45ngineResponse\x12\x19\n\x04spec\x18\x01 \x01(\x0b\x32\x0b.EngineSpec\x12\x1d\n\x06status\x18\x02 \x01(\x0b\x32\r.EngineStatus\x12\x0f\n\x07\x64\x65leted\x18\x03 \x01(\x08\"0\n\x13\x45ngineCreateRequest\x12\x19\n\x04spec\x18\x01 \x01(\x0b\x32\x0b.EngineSpec\"\x1d\n\rEngineRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"1\n\x14\x45ngineUpgradeRequest\x12\x19\n\x04spec\x18\x01 \x01(\x0b\x32\x0b.EngineSpec\"\x88\x01\n\x12\x45ngineListResponse\x12\x31\n\x07\x65ngines\x18\x01 \x03(\x0b\x32 .EngineListResponse.EnginesEntry\x1a?\n\x0c\x45nginesEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x1e\n\x05value\x18\x02 \x01(\x0b\x32\x0f.EngineResponse:\x02\x38\x01\"6\n\x14\x46rontendStartRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08\x66rontend\x18\x02 \x01(\t\"`\n\x0bProcessSpec\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0e\n\x06\x62inary\x18\x02 \x01(\t\x12\x0c\n\x04\x61rgs\x18\x03 \x03(\t\x12\x12\n\nport_count\x18\x04 \x01(\x05\x12\x11\n\tport_args\x18\x05 \x03(\t\"W\n\rProcessStatus\x12\r\n\x05state\x18\x01 \x01(\t\x12\x11\n\terror_msg\x18\x02 \x01(\t\x12\x12\n\nport_start\x18\x03 \x01(\x05\x12\x10\n\x08port_end\x18\x04 \x01(\x05\"2\n\x14ProcessCreateRequest\x12\x1a\n\x04spec\x18\x01 \x01(\x0b\x32\x0c.ProcessSpec\"$\n\x14ProcessDeleteRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"!\n\x11ProcessGetRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"^\n\x0fProcessResponse\x12\x1a\n\x04spec\x18\x01 \x01(\x0b\x32\x0c.ProcessSpec\x12\x1e\n\x06status\x18\x02 \x01(\x0b\x32\x0e.ProcessStatus\x12\x0f\n\x07\x64\x65leted\x18\x03 \x01(\x08\"\x14\n\x12ProcessListRequest\"\x91\x01\n\x13ProcessListResponse\x12\x36\n\tprocesses\x18\x01 \x03(\x0b\x32#.ProcessListResponse.ProcessesEntry\x1a\x42\n\x0eProcessesEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x1f\n\x05value\x18\x02 \x01(\x0b\x32\x10.ProcessResponse:\x02\x38\x01\"\x1a\n\nLogRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"\x1b\n\x0bLogResponse\x12\x0c\n\x04line\x18\x02 \x01(\t2\x9b\x05\n\x14\x45ngineManagerService\x12\x37\n\x0c\x45ngineCreate\x12\x14.EngineCreateRequest\x1a\x0f.EngineResponse\"\x00\x12\x31\n\x0c\x45ngineDelete\x12\x0e.EngineRequest\x1a\x0f.EngineResponse\"\x00\x12.\n\tEngineGet\x12\x0e.EngineRequest\x1a\x0f.EngineResponse\"\x00\x12;\n\nEngineList\x12\x16.google.protobuf.Empty\x1a\x13.EngineListResponse\"\x00\x12\x39\n\rEngineUpgrade\x12\x15.EngineUpgradeRequest\x1a\x0f.EngineResponse\"\x00\x12*\n\tEngineLog\x12\x0b.LogRequest\x1a\x0c.LogResponse\"\x00\x30\x01\x12:\n\x0b\x45ngineWatch\x12\x16.google.protobuf.Empty\x1a\x0f.EngineResponse\"\x00\x30\x01\x12@\n\rFrontendStart\x12\x15.FrontendStartRequest\x1a\x16.google.protobuf.Empty\"\x00\x12<\n\x10\x46rontendShutdown\x12\x0e.EngineRequest\x1a\x16.google.protobuf.Empty\"\x00\x12\x41\n\x15\x46rontendStartCallback\x12\x0e.EngineRequest\x1a\x16.google.protobuf.Empty\"\x00\x12\x44\n\x18\x46rontendShutdownCallback\x12\x0e.EngineRequest\x1a\x16.google.protobuf.Empty\"\x00\x32\xec\x02\n\x15ProcessManagerService\x12:\n\rProcessCreate\x12\x15.ProcessCreateRequest\x1a\x10.ProcessResponse\"\x00\x12:\n\rProcessDelete\x12\x15.ProcessDeleteRequest\x1a\x10.ProcessResponse\"\x00\x12\x34\n\nProcessGet\x12\x12.ProcessGetRequest\x1a\x10.ProcessResponse\"\x00\x12:\n\x0bProcessList\x12\x13.ProcessListRequest\x1a\x14.ProcessListResponse\"\x00\x12+\n\nProcessLog\x12\x0b.LogRequest\x1a\x0c.LogResponse\"\x00\x30\x01\x12<\n\x0cProcessWatch\x12\x16.google.protobuf.Empty\x1a\x10.ProcessResponse\"\x00\x30\x01\x62\x06proto3')
+  serialized_pb=_b('\n\trpc.proto\x1a\x1bgoogle/protobuf/empty.proto\"\xa6\x01\n\nEngineSpec\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x13\n\x0bvolume_name\x18\x02 \x01(\t\x12\x0e\n\x06\x62inary\x18\x03 \x01(\t\x12\x0e\n\x06listen\x18\x04 \x01(\t\x12\x11\n\tlisten_ip\x18\x05 \x01(\t\x12\x0c\n\x04size\x18\x06 \x01(\x03\x12\x10\n\x08\x66rontend\x18\x07 \x01(\t\x12\x10\n\x08\x62\x61\x63kends\x18\x08 \x03(\t\x12\x10\n\x08replicas\x18\t \x03(\t\"H\n\x0c\x45ngineStatus\x12&\n\x0eprocess_status\x18\x01 \x01(\x0b\x32\x0e.ProcessStatus\x12\x10\n\x08\x65ndpoint\x18\x02 \x01(\t\"[\n\x0e\x45ngineResponse\x12\x19\n\x04spec\x18\x01 \x01(\x0b\x32\x0b.EngineSpec\x12\x1d\n\x06status\x18\x02 \x01(\x0b\x32\r.EngineStatus\x12\x0f\n\x07\x64\x65leted\x18\x03 \x01(\x08\"0\n\x13\x45ngineCreateRequest\x12\x19\n\x04spec\x18\x01 \x01(\x0b\x32\x0b.EngineSpec\"\x1d\n\rEngineRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"1\n\x14\x45ngineUpgradeRequest\x12\x19\n\x04spec\x18\x01 \x01(\x0b\x32\x0b.EngineSpec\"\x88\x01\n\x12\x45ngineListResponse\x12\x31\n\x07\x65ngines\x18\x01 \x03(\x0b\x32 .EngineListResponse.EnginesEntry\x1a?\n\x0c\x45nginesEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x1e\n\x05value\x18\x02 \x01(\x0b\x32\x0f.EngineResponse:\x02\x38\x01\"1\n\x13\x45ngineExpandRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04size\x18\x02 \x01(\x03\"6\n\x14\x46rontendStartRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08\x66rontend\x18\x02 \x01(\t\"`\n\x0bProcessSpec\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0e\n\x06\x62inary\x18\x02 \x01(\t\x12\x0c\n\x04\x61rgs\x18\x03 \x03(\t\x12\x12\n\nport_count\x18\x04 \x01(\x05\x12\x11\n\tport_args\x18\x05 \x03(\t\"W\n\rProcessStatus\x12\r\n\x05state\x18\x01 \x01(\t\x12\x11\n\terror_msg\x18\x02 \x01(\t\x12\x12\n\nport_start\x18\x03 \x01(\x05\x12\x10\n\x08port_end\x18\x04 \x01(\x05\"2\n\x14ProcessCreateRequest\x12\x1a\n\x04spec\x18\x01 \x01(\x0b\x32\x0c.ProcessSpec\"$\n\x14ProcessDeleteRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"!\n\x11ProcessGetRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"^\n\x0fProcessResponse\x12\x1a\n\x04spec\x18\x01 \x01(\x0b\x32\x0c.ProcessSpec\x12\x1e\n\x06status\x18\x02 \x01(\x0b\x32\x0e.ProcessStatus\x12\x0f\n\x07\x64\x65leted\x18\x03 \x01(\x08\"\x14\n\x12ProcessListRequest\"\x91\x01\n\x13ProcessListResponse\x12\x36\n\tprocesses\x18\x01 \x03(\x0b\x32#.ProcessListResponse.ProcessesEntry\x1a\x42\n\x0eProcessesEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x1f\n\x05value\x18\x02 \x01(\x0b\x32\x10.ProcessResponse:\x02\x38\x01\"\x1a\n\nLogRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"\x1b\n\x0bLogResponse\x12\x0c\n\x04line\x18\x02 \x01(\t2\xd4\x05\n\x14\x45ngineManagerService\x12\x37\n\x0c\x45ngineCreate\x12\x14.EngineCreateRequest\x1a\x0f.EngineResponse\"\x00\x12\x31\n\x0c\x45ngineDelete\x12\x0e.EngineRequest\x1a\x0f.EngineResponse\"\x00\x12.\n\tEngineGet\x12\x0e.EngineRequest\x1a\x0f.EngineResponse\"\x00\x12;\n\nEngineList\x12\x16.google.protobuf.Empty\x1a\x13.EngineListResponse\"\x00\x12\x39\n\rEngineUpgrade\x12\x15.EngineUpgradeRequest\x1a\x0f.EngineResponse\"\x00\x12*\n\tEngineLog\x12\x0b.LogRequest\x1a\x0c.LogResponse\"\x00\x30\x01\x12:\n\x0b\x45ngineWatch\x12\x16.google.protobuf.Empty\x1a\x0f.EngineResponse\"\x00\x30\x01\x12\x37\n\x0c\x45ngineExpand\x12\x14.EngineExpandRequest\x1a\x0f.EngineResponse\"\x00\x12@\n\rFrontendStart\x12\x15.FrontendStartRequest\x1a\x16.google.protobuf.Empty\"\x00\x12<\n\x10\x46rontendShutdown\x12\x0e.EngineRequest\x1a\x16.google.protobuf.Empty\"\x00\x12\x41\n\x15\x46rontendStartCallback\x12\x0e.EngineRequest\x1a\x16.google.protobuf.Empty\"\x00\x12\x44\n\x18\x46rontendShutdownCallback\x12\x0e.EngineRequest\x1a\x16.google.protobuf.Empty\"\x00\x32\xec\x02\n\x15ProcessManagerService\x12:\n\rProcessCreate\x12\x15.ProcessCreateRequest\x1a\x10.ProcessResponse\"\x00\x12:\n\rProcessDelete\x12\x15.ProcessDeleteRequest\x1a\x10.ProcessResponse\"\x00\x12\x34\n\nProcessGet\x12\x12.ProcessGetRequest\x1a\x10.ProcessResponse\"\x00\x12:\n\x0bProcessList\x12\x13.ProcessListRequest\x1a\x14.ProcessListResponse\"\x00\x12+\n\nProcessLog\x12\x0b.LogRequest\x1a\x0c.LogResponse\"\x00\x30\x01\x12<\n\x0cProcessWatch\x12\x16.google.protobuf.Empty\x1a\x10.ProcessResponse\"\x00\x30\x01\x62\x06proto3')
   ,
   dependencies=[google_dot_protobuf_dot_empty__pb2.DESCRIPTOR,])
 
@@ -359,6 +359,44 @@ _ENGINELISTRESPONSE = _descriptor.Descriptor(
 )
 
 
+_ENGINEEXPANDREQUEST = _descriptor.Descriptor(
+  name='EngineExpandRequest',
+  full_name='EngineExpandRequest',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='name', full_name='EngineExpandRequest.name', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='size', full_name='EngineExpandRequest.size', index=1,
+      number=2, type=3, cpp_type=2, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=649,
+  serialized_end=698,
+)
+
+
 _FRONTENDSTARTREQUEST = _descriptor.Descriptor(
   name='FrontendStartRequest',
   full_name='FrontendStartRequest',
@@ -392,8 +430,8 @@ _FRONTENDSTARTREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=649,
-  serialized_end=703,
+  serialized_start=700,
+  serialized_end=754,
 )
 
 
@@ -451,8 +489,8 @@ _PROCESSSPEC = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=705,
-  serialized_end=801,
+  serialized_start=756,
+  serialized_end=852,
 )
 
 
@@ -503,8 +541,8 @@ _PROCESSSTATUS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=803,
-  serialized_end=890,
+  serialized_start=854,
+  serialized_end=941,
 )
 
 
@@ -534,8 +572,8 @@ _PROCESSCREATEREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=892,
-  serialized_end=942,
+  serialized_start=943,
+  serialized_end=993,
 )
 
 
@@ -565,8 +603,8 @@ _PROCESSDELETEREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=944,
-  serialized_end=980,
+  serialized_start=995,
+  serialized_end=1031,
 )
 
 
@@ -596,8 +634,8 @@ _PROCESSGETREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=982,
-  serialized_end=1015,
+  serialized_start=1033,
+  serialized_end=1066,
 )
 
 
@@ -641,8 +679,8 @@ _PROCESSRESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1017,
-  serialized_end=1111,
+  serialized_start=1068,
+  serialized_end=1162,
 )
 
 
@@ -665,8 +703,8 @@ _PROCESSLISTREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1113,
-  serialized_end=1133,
+  serialized_start=1164,
+  serialized_end=1184,
 )
 
 
@@ -703,8 +741,8 @@ _PROCESSLISTRESPONSE_PROCESSESENTRY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1215,
-  serialized_end=1281,
+  serialized_start=1266,
+  serialized_end=1332,
 )
 
 _PROCESSLISTRESPONSE = _descriptor.Descriptor(
@@ -733,8 +771,8 @@ _PROCESSLISTRESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1136,
-  serialized_end=1281,
+  serialized_start=1187,
+  serialized_end=1332,
 )
 
 
@@ -764,8 +802,8 @@ _LOGREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1283,
-  serialized_end=1309,
+  serialized_start=1334,
+  serialized_end=1360,
 )
 
 
@@ -795,8 +833,8 @@ _LOGRESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1311,
-  serialized_end=1338,
+  serialized_start=1362,
+  serialized_end=1389,
 )
 
 _ENGINESTATUS.fields_by_name['process_status'].message_type = _PROCESSSTATUS
@@ -820,6 +858,7 @@ DESCRIPTOR.message_types_by_name['EngineCreateRequest'] = _ENGINECREATEREQUEST
 DESCRIPTOR.message_types_by_name['EngineRequest'] = _ENGINEREQUEST
 DESCRIPTOR.message_types_by_name['EngineUpgradeRequest'] = _ENGINEUPGRADEREQUEST
 DESCRIPTOR.message_types_by_name['EngineListResponse'] = _ENGINELISTRESPONSE
+DESCRIPTOR.message_types_by_name['EngineExpandRequest'] = _ENGINEEXPANDREQUEST
 DESCRIPTOR.message_types_by_name['FrontendStartRequest'] = _FRONTENDSTARTREQUEST
 DESCRIPTOR.message_types_by_name['ProcessSpec'] = _PROCESSSPEC
 DESCRIPTOR.message_types_by_name['ProcessStatus'] = _PROCESSSTATUS
@@ -889,6 +928,13 @@ EngineListResponse = _reflection.GeneratedProtocolMessageType('EngineListRespons
   })
 _sym_db.RegisterMessage(EngineListResponse)
 _sym_db.RegisterMessage(EngineListResponse.EnginesEntry)
+
+EngineExpandRequest = _reflection.GeneratedProtocolMessageType('EngineExpandRequest', (_message.Message,), {
+  'DESCRIPTOR' : _ENGINEEXPANDREQUEST,
+  '__module__' : 'rpc_pb2'
+  # @@protoc_insertion_point(class_scope:EngineExpandRequest)
+  })
+_sym_db.RegisterMessage(EngineExpandRequest)
 
 FrontendStartRequest = _reflection.GeneratedProtocolMessageType('FrontendStartRequest', (_message.Message,), {
   'DESCRIPTOR' : _FRONTENDSTARTREQUEST,
@@ -985,8 +1031,8 @@ _ENGINEMANAGERSERVICE = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   serialized_options=None,
-  serialized_start=1341,
-  serialized_end=2008,
+  serialized_start=1392,
+  serialized_end=2116,
   methods=[
   _descriptor.MethodDescriptor(
     name='EngineCreate',
@@ -1052,9 +1098,18 @@ _ENGINEMANAGERSERVICE = _descriptor.ServiceDescriptor(
     serialized_options=None,
   ),
   _descriptor.MethodDescriptor(
+    name='EngineExpand',
+    full_name='EngineManagerService.EngineExpand',
+    index=7,
+    containing_service=None,
+    input_type=_ENGINEEXPANDREQUEST,
+    output_type=_ENGINERESPONSE,
+    serialized_options=None,
+  ),
+  _descriptor.MethodDescriptor(
     name='FrontendStart',
     full_name='EngineManagerService.FrontendStart',
-    index=7,
+    index=8,
     containing_service=None,
     input_type=_FRONTENDSTARTREQUEST,
     output_type=google_dot_protobuf_dot_empty__pb2._EMPTY,
@@ -1063,7 +1118,7 @@ _ENGINEMANAGERSERVICE = _descriptor.ServiceDescriptor(
   _descriptor.MethodDescriptor(
     name='FrontendShutdown',
     full_name='EngineManagerService.FrontendShutdown',
-    index=8,
+    index=9,
     containing_service=None,
     input_type=_ENGINEREQUEST,
     output_type=google_dot_protobuf_dot_empty__pb2._EMPTY,
@@ -1072,7 +1127,7 @@ _ENGINEMANAGERSERVICE = _descriptor.ServiceDescriptor(
   _descriptor.MethodDescriptor(
     name='FrontendStartCallback',
     full_name='EngineManagerService.FrontendStartCallback',
-    index=9,
+    index=10,
     containing_service=None,
     input_type=_ENGINEREQUEST,
     output_type=google_dot_protobuf_dot_empty__pb2._EMPTY,
@@ -1081,7 +1136,7 @@ _ENGINEMANAGERSERVICE = _descriptor.ServiceDescriptor(
   _descriptor.MethodDescriptor(
     name='FrontendShutdownCallback',
     full_name='EngineManagerService.FrontendShutdownCallback',
-    index=10,
+    index=11,
     containing_service=None,
     input_type=_ENGINEREQUEST,
     output_type=google_dot_protobuf_dot_empty__pb2._EMPTY,
@@ -1099,8 +1154,8 @@ _PROCESSMANAGERSERVICE = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=1,
   serialized_options=None,
-  serialized_start=2011,
-  serialized_end=2375,
+  serialized_start=2119,
+  serialized_end=2483,
   methods=[
   _descriptor.MethodDescriptor(
     name='ProcessCreate',
