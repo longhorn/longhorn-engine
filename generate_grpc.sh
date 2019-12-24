b8d52ff0f7af041779a8bf724311f06c1183e093
meldafrawi <mohamed.eldafrawi@rancher.com>
2019-12-24 22:30:48 +0200

    py3 upgrade: upgrade dependencies
    Signed-off-by: meldafrawi <mohamed.eldafrawi@rancher.com>

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -12,15 +12,15 @@ fi
 
 # controller
 protoc -I pkg/engine/controller/rpc/pb -I vendor_proto/protobuf/src/ pkg/engine/controller/rpc/pb/controller.proto --go_out=plugins=grpc:pkg/engine/controller/rpc/pb
-python -m grpc_tools.protoc -I pkg/engine/controller/rpc/pb -I vendor_proto/protobuf/src/ --python_out=integration/rpc/controller --grpc_python_out=integration/rpc/controller pkg/engine/controller/rpc/pb/controller.proto
+python3 -m grpc_tools.protoc -I pkg/engine/controller/rpc/pb -I vendor_proto/protobuf/src/ --python_out=integration/rpc/controller --grpc_python_out=integration/rpc/controller pkg/engine/controller/rpc/pb/controller.proto
 
 # replica
 protoc -I pkg/engine/replica/rpc -I vendor_proto/protobuf/src/ pkg/engine/replica/rpc/replica.proto --go_out=plugins=grpc:pkg/engine/replica/rpc
-python -m grpc_tools.protoc -I pkg/engine/replica/rpc -I vendor_proto/protobuf/src/ --python_out=integration/rpc/replica --grpc_python_out=integration/rpc/replica pkg/engine/replica/rpc/replica.proto
+python3 -m grpc_tools.protoc -I pkg/engine/replica/rpc -I vendor_proto/protobuf/src/ --python_out=integration/rpc/replica --grpc_python_out=integration/rpc/replica pkg/engine/replica/rpc/replica.proto
 
 # sync agent
 protoc -I pkg/engine/sync/rpc -I vendor_proto/protobuf/src/ pkg/engine/sync/rpc/rpc.proto --go_out=plugins=grpc:pkg/engine/sync/rpc
 
 # instance manager
-python -m grpc_tools.protoc -I pkg/instance-manager/rpc -I vendor_proto/protobuf/src/ --python_out=integration/rpc/instance_manager --grpc_python_out=integration/rpc/instance_manager pkg/instance-manager/rpc/rpc.proto
+python3 -m grpc_tools.protoc -I pkg/instance-manager/rpc -I vendor_proto/protobuf/src/ --python_out=integration/rpc/instance_manager --grpc_python_out=integration/rpc/instance_manager pkg/instance-manager/rpc/rpc.proto
 protoc -I pkg/instance-manager/rpc/ -I vendor_proto/protobuf/src/ pkg/instance-manager/rpc/rpc.proto --go_out=plugins=grpc:pkg/instance-manager/rpc
