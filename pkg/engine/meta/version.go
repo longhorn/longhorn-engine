76f7a0c095fc5e032675c7d0dc10b28086876bda
Shuo Wu <shuo@rancher.com>
2020-02-12 12:16:00 +0800

    Add more descriptions for the API versions
    Signed-off-by: Shuo Wu <shuo@rancher.com>

--- pkg/engine/meta/version.go
+++ pkg/engine/meta/version.go
@@ -1,11 +1,21 @@
 package meta
 
+// For the following API versions, if a new field/API is added and the gRPC/cli
+// backward compatibility is not broken, bumping up APIVersion is enough.
+// But if a existing field/API is modified or deleted, bumping up both APIVersion
+// and APIMinVersion are required.
 const (
 	// CLIAPIVersion used to communicate with user e.g. longhorn-manager
+	// These 2 version numbers involves in the common volume operation commands,
+	// e.g. snapshot and backup.
 	CLIAPIVersion    = 3
 	CLIAPIMinVersion = 3
 
-	// ControllerAPIVersion used to communicate with instance-manager
+	// ControllerAPIVersion used to indicate the current live upgrade mechanism.
+	// And the live upgrade mechanism is related to the instance-manager process
+	// replacement and the engine/replica starting command.
+	// Besides, ControllerAPIMinVersion needs to be bumped up once
+	// InstanceManagerAPIMinVersion is bumped up.
 	ControllerAPIVersion    = 3
 	ControllerAPIMinVersion = 3
 
@@ -13,8 +23,12 @@ const (
 	DataFormatVersion    = 1
 	DataFormatMinVersion = 1
 
-	// InstanceManagerAPIVersion used to communicate with user e.g. longhorn-manager
-	InstanceManagerAPIVersion    = 1
+	// InstanceManagerAPIVersion used to communicate with user e.g.longhorn-manager.
+	// It indicates the current API version of the instance-manager gRPC server.
+	// These 2 version numbers rely on the instance-manager gRPC definition.
+	InstanceManagerAPIVersion = 1
+	// InstanceManagerAPIMinVersion indicates the minimal version that this engine can share
+	// the instance-manager with other engines.
 	InstanceManagerAPIMinVersion = 1
 )
 
