0b0c8202075f29d93d94c1252ab77833c9fb873e
Shuo Wu <shuo.wu@suse.com>
2021-06-20 14:56:40 +0800

    meta: Bump up the CLI API version by 1 after updating the backup creation API
    Longhorn #2530
    
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- pkg/meta/version.go
+++ pkg/meta/version.go
@@ -2,7 +2,7 @@ package meta
 
 const (
 	// CLIAPIVersion used to communicate with user e.g. longhorn-manager
-	CLIAPIVersion    = 4
+	CLIAPIVersion    = 5
 	CLIAPIMinVersion = 3
 
 	// ControllerAPIVersion used to communicate with instance-manager
