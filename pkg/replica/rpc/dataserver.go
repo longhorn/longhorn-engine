e5ef26f75f1c636a9339fc689da34802cb2c6649
Keith Lucas <keith.lucas@suse.com>
2021-11-09 15:21:22 -0500

    Restrict data port connections.
    Only allow one TCP connection to a replica data port and verify that the
    host of each connection matches the specified host suffix.
    
    Signed-off-by: Keith Lucas <keith.lucas@suse.com>

--- pkg/replica/rpc/dataserver.go
+++ pkg/replica/rpc/dataserver.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"net"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 
@@ -10,15 +11,43 @@ import (
 )
 
 type DataServer struct {
-	address string
-	s       *replica.Server
+	address    string
+	s          *replica.Server
+	hostSuffix string
 }
 
-func NewDataServer(address string, s *replica.Server) *DataServer {
+func NewDataServer(address string, s *replica.Server, hostSuffix string) *DataServer {
+
 	return &DataServer{
-		address: address,
-		s:       s,
+		address:    address,
+		s:          s,
+		hostSuffix: hostSuffix,
 	}
+}
+
+func (s *DataServer) verifyIP(addr net.Addr) bool {
+	// If the host suffix wasn't provided, allow all connections.
+	if len(s.hostSuffix) == 0 {
+		return true
+	}
+
+	remoteIP, _, err := net.SplitHostPort(addr.String())
+
+	domains, err := net.LookupAddr(remoteIP)
+
+	if err != nil {
+		logrus.Errorf("unable to resolve %v: %v ", remoteIP, err)
+
+		return true
+	}
+
+	for _, domain := range domains {
+		if strings.HasSuffix(domain, s.hostSuffix) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (s *DataServer) ListenAndServe() error {
@@ -39,11 +68,20 @@ func (s *DataServer) ListenAndServe() error {
 			continue
 		}
 
-		logrus.Infof("New connection from: %v", conn.RemoteAddr())
+		remoteAddr := conn.RemoteAddr()
 
-		go func(conn net.Conn) {
-			server := dataconn.NewServer(conn, s.s)
-			server.Handle()
-		}(conn)
+		logrus.Infof("New connection from: %v", remoteAddr)
+
+		if !s.verifyIP(remoteAddr) {
+			logrus.Errorf("%v is not allowed to connect", remoteAddr)
+			conn.Close()
+			continue
+		}
+
+		server := dataconn.NewServer(conn, s.s)
+
+		server.Handle()
+
+		logrus.Infof("Lost connection from: %v", conn.RemoteAddr())
 	}
 }
