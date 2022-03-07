fbd73720af5b61c55d2791924e4d0e3e76946abd
Shuo Wu <shuo.wu@suse.com>
2022-03-07 20:14:48 +0800

    vendor: Update longhorn/sparse-tools
    Longhorn 3613
    
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- vendor/github.com/longhorn/sparse-tools/sparse/util.go
+++ vendor/github.com/longhorn/sparse-tools/sparse/util.go
@@ -3,7 +3,47 @@ package sparse
 import (
 	"context"
 	"sync"
+	"sync/atomic"
+	"syscall"
 )
+
+const (
+	batchBlockCount  = 32
+	progressComplete = uint32(100)
+)
+
+type FileHandlingOperations interface {
+	UpdateFileHandlingProgress(progress int, done bool, err error)
+}
+
+// updateProgress increases progress monotonically only.
+// Once that's replaced we can use an atomic cmp&swp around progress that
+// doesn't require locking.
+// We also limit the update function calls to p=[0,100] since there
+// is no point in calling updates for the same p value multiple times
+func updateProgress(currentProgressAddr *uint32, newProgress uint32, done bool, err error, progressMutex *sync.Mutex, ops FileHandlingOperations) {
+	forcedUpdate := done || err != nil
+	if !forcedUpdate && newProgress <= atomic.LoadUint32(currentProgressAddr) {
+		return
+	}
+
+	// this lock ensures that there is only a single in flight UpdateFileHandlingProgress operation
+	// with multiple in flight operations, the lock on the receiver side would be non-deterministically
+	// acquired which would potentially lead to non-monotonic progress updates.
+	progressMutex.Lock()
+	defer progressMutex.Unlock()
+	if forcedUpdate || newProgress > atomic.LoadUint32(currentProgressAddr) {
+		atomic.StoreUint32(currentProgressAddr, newProgress)
+		ops.UpdateFileHandlingProgress(int(newProgress), done, err)
+	}
+}
+
+// get the file system block size
+func getFileSystemBlockSize(fileIo FileIoProcessor) (int, error) {
+	var stat syscall.Stat_t
+	err := syscall.Stat(fileIo.Name(), &stat)
+	return int(stat.Blksize), err
+}
 
 // mergeErrorChannels will merge all error channels into a single error out channel.
 // the error out channel will be closed once the ctx is done or all error channels are closed
