8e57ec046e2bc006cb6277bcd07355864951f480
Phan Le <phan.le@suse.com>
2022-04-21 23:34:07 -0700

    Fix bug forget to remove the snapshot from r.diskChildrenMap when removing snapshot
    Longhorn-3883
    
    Signed-off-by: Phan Le <phan.le@suse.com>

--- pkg/replica/replica.go
+++ pkg/replica/replica.go
@@ -493,6 +493,7 @@ func (r *Replica) removeDiskNode(name string, force bool) error {
 		return err
 	}
 	delete(r.diskData, name)
+	delete(r.diskChildrenMap, name)
 
 	index := r.findDisk(name)
 	if index <= 0 {
