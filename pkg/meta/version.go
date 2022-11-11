8f19a15e35172c33f07e4ee763dd6d0fafdce9fc
Derek Su <derek.su@suse.com>
2022-11-11 16:28:08 +0800

    meta: bump api and cli version
    Longhorn 4491
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- pkg/meta/version.go
+++ pkg/meta/version.go
@@ -2,7 +2,7 @@ package meta
 
 const (
 	// CLIAPIVersion used to communicate with user e.g. longhorn-manager
-	CLIAPIVersion    = 6
+	CLIAPIVersion    = 7
 	CLIAPIMinVersion = 3
 
 	// ControllerAPIVersion used to communicate with instance-manager
