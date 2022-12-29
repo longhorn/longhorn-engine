87ce1b1a155fa231724f0f57892555a58f9a99b5
Derek Su <derek.su@suse.com>
2022-12-29 16:56:44 +0800

    Ignore ENOEXIST if file is not existing in rmDisk
    Longhorn 5169
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- pkg/replica/replica.go
+++ pkg/replica/replica.go
@@ -804,12 +804,12 @@ func (r *Replica) rmDisk(name string) error {
 	logrus.Infof("Removing disk %v", name)
 
 	diskPath := r.diskPath(name)
-	lastErr := os.Remove(diskPath)
+	lastErr := os.RemoveAll(diskPath)
 	if lastErr != nil {
 		logrus.WithError(lastErr).Errorf("Failed to remove disk file %v", diskPath)
 	}
 	diskMetaPath := r.diskPath(name + diskutil.DiskMetadataSuffix)
-	if err := os.Remove(diskMetaPath); err != nil {
+	if err := os.RemoveAll(diskMetaPath); err != nil {
 		lastErr = err
 		logrus.WithError(lastErr).Errorf("Failed to remove disk metadata file %v", diskMetaPath)
 	}
@@ -891,7 +891,7 @@ func (r *Replica) createDisk(name string, userCreated bool, created string, labe
 		}
 
 		var rollbackErr error
-		log.WithError(err).Errorf("failed to create disk %v, will do rollback", name)
+		log.WithError(err).Errorf("Failed to create disk %v, will do rollback", name)
 		for _, rollbackFunc := range rollbackFuncList {
 			if rollbackFunc == nil {
 				continue
