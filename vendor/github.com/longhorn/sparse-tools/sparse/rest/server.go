ffcfcb5253cd2fa1c0065803e92bc68cc697f5c4
Jack Lin <jack.lin@suse.com>
2023-04-10 11:53:05 +0800

    vendor: update sparse-tools
    Ref: longhorn/longhorn 4305
    
    Signed-off-by: Jack Lin <jack.lin@suse.com>

--- vendor/github.com/longhorn/sparse-tools/sparse/rest/server.go
+++ vendor/github.com/longhorn/sparse-tools/sparse/rest/server.go
@@ -2,14 +2,57 @@ package rest
 
 import (
 	"context"
+	"net"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/longhorn/sparse-tools/sparse"
 )
+
+var (
+	defaultIdleTimeout     = 90 * time.Second
+	defaultHTTPIdleTimeout = 60 * time.Second
+)
+
+type IdleTimer struct {
+	sync.Mutex
+	activeConns map[net.Conn]bool
+	timeout     time.Duration
+	timer       *time.Timer
+}
+
+func NewIdleTimer(idleTimeout time.Duration) *IdleTimer {
+	return &IdleTimer{
+		activeConns: make(map[net.Conn]bool),
+		timeout:     idleTimeout,
+		timer:       time.NewTimer(idleTimeout),
+	}
+}
+
+func (it *IdleTimer) ConnState(conn net.Conn, state http.ConnState) {
+	it.Lock()
+	defer it.Unlock()
+
+	switch state {
+	case http.StateNew, http.StateActive, http.StateHijacked:
+		it.activeConns[conn] = true
+		it.timer.Stop()
+	case http.StateIdle, http.StateClosed:
+		delete(it.activeConns, conn)
+		if len(it.activeConns) == 0 {
+			it.timer.Reset(it.timeout)
+		}
+	}
+}
+
+func (t *IdleTimer) Done() <-chan time.Time {
+	return t.timer.C
+}
 
 type DataSyncServer interface {
 	open(writer http.ResponseWriter, request *http.Request)
@@ -36,8 +79,13 @@ func Server(ctx context.Context, port string, filePath string, syncFileOps SyncF
 	log.Infof("Creating Ssync service")
 	ctx, cancelFunc := context.WithCancel(ctx)
 	srv := &http.Server{
-		Addr: ":" + port,
+		Addr:        ":" + port,
+		IdleTimeout: defaultHTTPIdleTimeout,
 	}
+
+	// if server has no connection for a period of time, it will shutdown itself to prevent receiver from getting stuck.
+	idleTimer := NewIdleTimer(defaultIdleTimeout)
+	srv.ConnState = idleTimer.ConnState
 
 	fileAlreadyExists := true
 	if _, err := os.Stat(filePath); err != nil && errors.Is(err, os.ErrNotExist) {
@@ -56,8 +104,13 @@ func Server(ctx context.Context, port string, filePath string, syncFileOps SyncF
 	srv.Handler = NewRouter(syncServer)
 
 	go func() {
-		<-ctx.Done()
-		srv.Close()
+		select {
+		case <-ctx.Done():
+			srv.Close()
+		case <-idleTimer.Done():
+			log.Errorf("Shutting down the server since it is idle for %v", idleTimer.timeout)
+			srv.Close()
+		}
 	}()
 
 	return srv.ListenAndServe()
