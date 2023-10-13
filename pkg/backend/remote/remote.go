80dedf1961702923e5f2ef61727436b55fdc9e39
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-10-13 16:26:36 +0800

    refactor: replace fmt.Errorf with errors.Wrapf
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>
    (cherry picked from commit 95b3fb85c9a415167e076a803964656e356a9f20)

--- pkg/backend/remote/remote.go
+++ pkg/backend/remote/remote.go
@@ -258,7 +258,7 @@ func (r *Remote) SetUnmapMarkSnapChainRemoved(enabled bool) error {
 	conn, err := grpc.Dial(r.replicaServiceURL, grpc.WithInsecure(),
 		ptypes.WithIdentityValidationClientInterceptor(r.volumeName, ""))
 	if err != nil {
-		return fmt.Errorf("cannot connect to ReplicaService %v: %v", r.replicaServiceURL, err)
+		return errors.Wrapf(err, "failed connecting to ReplicaService %v", r.replicaServiceURL)
 	}
 	defer conn.Close()
 	replicaServiceClient := ptypes.NewReplicaServiceClient(conn)
