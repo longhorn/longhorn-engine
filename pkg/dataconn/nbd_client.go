2be76929ae8c533d144513645020d13ef5af8b0b
Toutou98 <miketoutou@gmail.com>
2024-03-26 21:38:23 +0200

    Replaced locks with chans. Added error messages on ReadAt/WriteAt

--- pkg/dataconn/nbd_client.go
+++ pkg/dataconn/nbd_client.go
@@ -2,28 +2,27 @@ package dataconn
 
 import (
 	"net"
-	"sync"
 	"time"
 
+	"github.com/sirupsen/logrus"
 	"libguestfs.org/libnbd"
 )
 
-type NbdClientWrapper struct {
+type NbdClientController struct {
 	handles        []*libnbd.Libnbd
 	conn           net.Conn
 	maxConnections int
-	next           int
-	indexLock      sync.Mutex
+	handleCh       chan *libnbd.Libnbd
 	end            chan struct{}
 	handlesAlive   chan struct{}
 }
 
-func NewNbdClientWrapper(conn net.Conn, engineToReplicaTimeout time.Duration, nbdEnabled int) *NbdClientWrapper {
-	wrapper := &NbdClientWrapper{
+func NewNbdClientWrapper(conn net.Conn, engineToReplicaTimeout time.Duration, nbdEnabled int) *NbdClientController {
+	wrapper := &NbdClientController{
 		handles:        make([]*libnbd.Libnbd, nbdEnabled),
 		conn:           conn,
 		maxConnections: nbdEnabled,
-		next:           0,
+		handleCh:       make(chan *libnbd.Libnbd, nbdEnabled),
 		end:            make(chan struct{}),
 		handlesAlive:   make(chan struct{}),
 	}
@@ -32,7 +31,7 @@ func NewNbdClientWrapper(conn net.Conn, engineToReplicaTimeout time.Duration, nb
 	return wrapper
 }
 
-func (w *NbdClientWrapper) handle() {
+func (w *NbdClientController) handle() {
 	for i := 0; i < w.maxConnections; i++ {
 		h, err := libnbd.Create()
 		if err != nil {
@@ -45,48 +44,120 @@ func (w *NbdClientWrapper) handle() {
 		if err != nil {
 			panic(err)
 		}
+		w.handleCh <- h
 	}
 	w.handlesAlive <- struct{}{}
-	for {
-		<-w.end
-		return
-	}
+	<-w.end
 }
 
-func (w *NbdClientWrapper) ReadAt(buf []byte, offset int64) (int, error) {
-	w.indexLock.Lock()
-	w.next = (w.next + 1) % w.maxConnections
-	index := w.next
-	w.indexLock.Unlock()
-	err := w.handles[index].Pread(buf, uint64(offset), nil)
+func (w *NbdClientController) ReadAt(buf []byte, offset int64) (int, error) {
+	h := <-w.handleCh
+	err := h.Pread(buf, uint64(offset), nil)
+	w.handleCh <- h
 	if err != nil {
+		logrus.WithError(err).Error("Failed to read from NBD server: ", w.conn.RemoteAddr().String())
 		return 0, err
 	}
 
 	return len(buf), nil
 }
 
-func (w *NbdClientWrapper) WriteAt(buf []byte, offset int64) (int, error) {
-	w.indexLock.Lock()
-	w.next = (w.next + 1) % w.maxConnections
-	index := w.next
-	w.indexLock.Unlock()
-	err := w.handles[index].Pwrite(buf, uint64(offset), nil)
+func (w *NbdClientController) WriteAt(buf []byte, offset int64) (int, error) {
+	h := <-w.handleCh
+	err := h.Pwrite(buf, uint64(offset), nil)
+	w.handleCh <- h
 	if err != nil {
+		logrus.WithError(err).Error("Failed to write to NBD server: ", w.conn.RemoteAddr().String())
 		return 0, err
 	}
 
 	return len(buf), nil
 }
 
-func (w *NbdClientWrapper) UnmapAt(length uint32, offset int64) (int, error) {
+func (w *NbdClientController) UnmapAt(length uint32, offset int64) (int, error) {
 	return int(length), nil
 
 }
 
-func (w *NbdClientWrapper) Close() {
+func (w *NbdClientController) Close() {
 	for i := 0; i < w.maxConnections; i++ {
 		w.handles[i].Close()
 	}
 	w.end <- struct{}{}
 }
+
+// type NbdClientWrapper struct {
+// 	handles        []*libnbd.Libnbd
+// 	conn           net.Conn
+// 	maxConnections int
+// 	next           int32
+// 	end            chan struct{}
+// 	handlesAlive   chan struct{}
+// }
+
+// func NewNbdClientWrapper(conn net.Conn, engineToReplicaTimeout time.Duration, nbdEnabled int) *NbdClientWrapper {
+// 	wrapper := &NbdClientWrapper{
+// 		handles:        make([]*libnbd.Libnbd, nbdEnabled),
+// 		conn:           conn,
+// 		maxConnections: nbdEnabled,
+// 		next:           0,
+// 		end:            make(chan struct{}),
+// 		handlesAlive:   make(chan struct{}),
+// 	}
+// 	go wrapper.handle()
+// 	<-wrapper.handlesAlive
+// 	return wrapper
+// }
+
+// func (w *NbdClientWrapper) handle() {
+// 	for i := 0; i < w.maxConnections; i++ {
+// 		h, err := libnbd.Create()
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		w.handles[i] = h
+// 		uri := "nbd://" + w.conn.RemoteAddr().String()
+// 		err = w.handles[i].ConnectUri(uri)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 	}
+// 	w.handlesAlive <- struct{}{}
+// 	for {
+// 		<-w.end
+// 		return
+// 	}
+// }
+
+// func (w *NbdClientWrapper) ReadAt(buf []byte, offset int64) (int, error) {
+// 	index := atomic.AddInt32(&w.next, 1) % int32(w.maxConnections)
+// 	err := w.handles[index].Pread(buf, uint64(offset), nil)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+
+// 	return len(buf), nil
+// }
+
+// func (w *NbdClientWrapper) WriteAt(buf []byte, offset int64) (int, error) {
+// 	index := atomic.AddInt32(&w.next, 1) % int32(w.maxConnections)
+// 	err := w.handles[index].Pwrite(buf, uint64(offset), nil)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+
+// 	return len(buf), nil
+// }
+
+// func (w *NbdClientWrapper) UnmapAt(length uint32, offset int64) (int, error) {
+// 	return int(length), nil
+
+// }
+
+// func (w *NbdClientWrapper) Close() {
+// 	for i := 0; i < w.maxConnections; i++ {
+// 		w.handles[i].Close()
+// 	}
+// 	w.end <- struct{}{}
+// }
