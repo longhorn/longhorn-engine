a0dada7dcb8ca47a6b697c2ed093b28758134391
Phan Le <phan.le@suse.com>
2024-05-28 15:23:08 -0700

    Fix bug the engine might choose a replica with a smaller head size to be the source of truth for auto-salvage
    longhorn-8659
    
    Signed-off-by: Phan Le <phan.le@suse.com>
    (cherry picked from commit fd668c94d325b86b2f2d8eeed0a91c48a7c0acf5)

--- pkg/controller/control.go
+++ pkg/controller/control.go
@@ -551,6 +551,7 @@ func (c *Controller) salvageRevisionCounterDisabledReplicas() error {
 		if t.Add(lastModifyCheckPeriod).After(lastTime) {
 			if salvageReplica.HeadFileSize >= largestSize {
 				bestCandidate = r
+				largestSize = salvageReplica.HeadFileSize
 			}
 		}
 	}
