6563c7b14dd07fdf58c270d40e895efcb9c56359
Raphanus Lo <yunchang.lo@suse.com>
2025-07-18 16:37:16 +0800

    feat(backup): configurable bkup block size
    longhorn-5215
    
    Signed-off-by: Raphanus Lo <yunchang.lo@suse.com>

--- pkg/replica/types.go
+++ pkg/replica/types.go
@@ -3,8 +3,6 @@ package replica
 type ProgressState string
 
 const (
-	backupBlockSize = 2 << 20 // 2MiB
-
 	ProgressStateInProgress = ProgressState("in_progress")
 	ProgressStateComplete   = ProgressState("complete")
 	ProgressStateError      = ProgressState("error")
