65deef008bf38f6f3b8f50178ad0d1cf4f214fae
Raphanus Lo <yunchang.lo@suse.com>
2025-09-05 15:14:08 +0800

    fix(replica): validate size with backing image virtual size
    longhorn-11362
    
    Signed-off-by: Raphanus Lo <yunchang.lo@suse.com>
    (cherry picked from commit ea41eff40c9317b16aa47b65ed9f754a309e67f8)

--- pkg/backingfile/backingfile.go
+++ pkg/backingfile/backingfile.go
@@ -1,15 +1,12 @@
 package backingfile
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"os"
-	"os/exec"
-	"strings"
-	"syscall"
 
 	"github.com/longhorn/sparse-tools/sparse"
+
+	"github.com/longhorn/go-common-libs/backingimage"
 
 	"github.com/longhorn/longhorn-engine/pkg/qcow"
 	"github.com/longhorn/longhorn-engine/pkg/types"
@@ -18,55 +15,11 @@ import (
 )
 
 type BackingFile struct {
-	Size       int64
-	SectorSize int64
-	Path       string
-	Disk       types.DiffDisk
-}
-
-func detectFileFormat(file string) (string, error) {
-
-	/* Example command outputs
-	   $ qemu-img info parrot.raw
-	   image: parrot.raw
-	   file format: raw
-	   virtual size: 32M (33554432 bytes)
-	   disk size: 2.2M
-
-	   $ qemu-img info parrot.qcow2
-	   image: parrot.qcow2
-	   file format: qcow2
-	   virtual size: 32M (33554432 bytes)
-	   disk size: 2.3M
-	   cluster_size: 65536
-	   Format specific information:
-	       compat: 1.1
-	       lazy refcounts: false
-	       refcount bits: 16
-	       corrupt: false
-	*/
-
-	cmd := exec.Command("qemu-img", "info", file)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Pdeathsig: syscall.SIGKILL,
-	}
-	var output, stderr bytes.Buffer
-	cmd.Stdout = &output
-	cmd.Stderr = &stderr
-	if err := cmd.Run(); err != nil {
-		return "", fmt.Errorf("failed to check backing file format with command [qume-img info %v], output %s, stderr, %s, error %v",
-			file, output.String(), stderr.String(), err)
-	}
-
-	scanner := bufio.NewScanner(strings.NewReader(output.String()))
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if strings.HasPrefix(line, "file format: ") {
-			return strings.TrimPrefix(line, "file format: "), nil
-		}
-	}
-
-	return "", fmt.Errorf("cannot find the file format in the output %s", output.String())
+	Size        int64
+	VirtualSize int64
+	SectorSize  int64
+	Path        string
+	Disk        types.DiffDisk
 }
 
 func OpenBackingFile(file string) (*BackingFile, error) {
@@ -78,13 +31,14 @@ func OpenBackingFile(file string) (*BackingFile, error) {
 		return nil, err
 	}
 
-	format, err := detectFileFormat(file)
+	imageToolExec := backingimage.NewQemuImgExecutor()
+	imageInfo, err := imageToolExec.GetImageInfo(file)
 	if err != nil {
 		return nil, err
 	}
 
 	var f types.DiffDisk
-	switch format {
+	switch imageInfo.Format {
 	case "qcow2":
 		if f, err = qcow.Open(file); err != nil {
 			return nil, err
@@ -94,7 +48,7 @@ func OpenBackingFile(file string) (*BackingFile, error) {
 			return nil, err
 		}
 	default:
-		return nil, fmt.Errorf("format %v of the backing file %v is not supported", format, file)
+		return nil, fmt.Errorf("format %v of the backing file %v is not supported", imageInfo.Format, file)
 	}
 
 	size, err := f.Size()
@@ -106,9 +60,10 @@ func OpenBackingFile(file string) (*BackingFile, error) {
 	}
 
 	return &BackingFile{
-		Path:       file,
-		Disk:       f,
-		Size:       size,
-		SectorSize: diskutil.BackingImageSectorSize,
+		Path:        file,
+		Disk:        f,
+		Size:        size,
+		VirtualSize: imageInfo.VirtualSize,
+		SectorSize:  diskutil.BackingImageSectorSize,
 	}, nil
 }
